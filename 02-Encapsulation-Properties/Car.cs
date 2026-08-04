6f6d0151e8f6345664de0890221332b0015ddcef
Pirvella <vakhtangpirveli@gmail.com>
2026-08-05 03:18:19 +0400

    Add encapsulation example with Car class and project setup
    Added a new project file (`02-Encapsulation-Properties.csproj`)
    and solution file (`02-Encapsulation-Properties.sln`) to set up
    the project targeting .NET 8.0.
    
    Implemented a `Car` class in `Car.cs` with encapsulated properties
    (`Brand`, `Model`, `Year`, `Speed`) and validation logic. Added
    methods (`Start`, `Drive`, `ShowInfo`) to simulate car behavior.
    
    Created a `Program` class in `Program.cs` to demonstrate the
    functionality of the `Car` class by instantiating an object,
    setting its properties, and calling its methods.

---
+++ 02-Encapsulation-Properties/Car.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Encapsulation_Properties
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        private int  year;
+
+
+
+        public int Year
+        {
+             get
+             {
+                return year;
+             }
+
+            set
+            {
+                if (value>=2000)
+                {
+                    year = value;
+                }
+                else
+                {
+                    Console.WriteLine("Year must be greater than or equal to 2000.");
+                }
+            }
+             
+
+
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value >= 0&& value <= 300)
+                {
+                    speed = value;
+                }
+                else
+                {
+                    Console.WriteLine("Speed cannot be negative or more 300.");
+                }
+            }
+        }
+
+        public void Start()
+        
+        {
+            Console.WriteLine ("The car is starting.");
+        }
+
+        public void Drive()
+
+        {
+            Console.WriteLine("The car is driving");
+
+        }
+
+        public void ShowInfo()
+        
+        {
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Speed: {Speed}");
+
+        }
+
+
+        // Encapsulation can also be implemented by restricting direct access to the property
+        // and controlling its value through a public method with validation.
+        //public void SetYear(int year)
+        //{
+        //    if (year >= 2000)
+        //    {
+        //        Year = year;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Year must be greater than or equal to 2000.");
+        //    }
+        //}
+
+
+    }
+}
---
+++ 02-Encapsulation-Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02_Encapsulation_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Brand = "Toyota";
+            car1.Model = "Camry";
+            car1.Year = 2020;
+            car1.Speed = 20;
+            car1.Start();
+            car1.Drive();
+                 //car1.SetYear(2020);
+            car1.ShowInfo();
+        }
+    }
+}
