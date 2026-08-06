2046e8f0ea7ef6c21f3daf03e1402ab5cdfe80a9
Pirvella <vakhtangpirveli@gmail.com>
2026-08-07 02:18:20 +0400

    Add Car class and project setup for constructors demo
    Added a new project file `03-Constructors-Destructors.csproj`
    and solution file `03-Constructors-Destructors.sln` to set up
    a .NET 8.0 project with nullable reference types enabled.
    
    Implemented a `Car` class in `Car.cs` with properties
    (`Brand`, `Model`, `Year`), a constructor, a destructor,
    and methods (`Start`, `Drive`, `ShowInfo`) to simulate car
    behavior.
    
    Added a `Program` class in `Program.cs` to demonstrate the
    usage of the `Car` class by creating two `Car` objects and
    calling their methods.

---
+++ 03-Constructors-Destructors/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Constructors_Destructors
+{
+    internal class Car
+    {
+       public string Brand { get; set; }
+       public string Model { get; set; }
+       public int Year { get; set; }
+
+        public Car(string brand, string model, int year)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+
+            
+        }
+
+        ~Car()
+        {
+            Console.WriteLine("The car object is being destroyed.");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("The car is starting.");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("The car is driving.");
+
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand:{Brand}");
+            Console.WriteLine($"Model:{Model}");
+            Console.WriteLine($"Year:{Year}");
+        }
+    }
+}
---
+++ 03-Constructors-Destructors/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_Constructors_Destructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Tesla", "Model S", 2022);
+            car1.Start();
+            car1.Drive();
+            car1.ShowInfo();
+
+            Car car2 = new Car("Lamborghini", "Urus", 2021);
+            car2.Start();
+            car2.Drive();
+            car2.ShowInfo();
+
+            //car1 = null;
+            
+
+        }
+
+    }
+}
